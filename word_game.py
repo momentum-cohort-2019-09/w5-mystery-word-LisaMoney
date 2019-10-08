cf9da2a500f916484bd339d2e190553cceecd0bb
Lisa Money <lisamoneylama@gmail.com>
2019-10-08 14:57:31 -0400

    Woohoo I have a word and it's length

--- word_game.py
+++ word_game.py
@@ -2,11 +2,18 @@ import random
 
 # intro = input("Welcome to the Mystery Word game.  This is a game where you will guess the letters to guess a word.  Are you ready to play?")
 
+# def get_length(sorted_word):
+#     word_length = len(random_word)
+#     print(word_length)
+#     return word_length
+
 def get_word(word_list):
     random_word = random.choice(word_list)
+    word_length = len(random_word)
+    print(word_length)
     print(random_word)
     return random_word
-
+    
 def make_list_from_text(words):
     list_of_words = words.split()
     return list_of_words
@@ -16,8 +23,8 @@ def read_file(filename):
         text = words.read()
         return make_list_from_text(text)
 
-get_word(read_file("words.txt"))
+# get_length(read_file(sorted_word))
 
-    
+get_word(read_file("words.txt"))
 
 #with open("words.txt") as words: for word of words pass
