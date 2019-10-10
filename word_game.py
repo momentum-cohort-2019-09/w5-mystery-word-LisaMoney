593da5e8034351f8423c428c21ab79ab253475f0
Lisa Money <lisamoneylama@gmail.com>
2019-10-10 17:16:29 -0400

    Things are now displaying in order.

--- word_game.py
+++ word_game.py
@@ -1,27 +1,44 @@
 import random
 
-def info_for_user():
-    intro = input("Welcome to the Mystery Word game.  This is a game where you will guess the letters to guess a word.  Are you ready to play?")
-    print(info_for_user)
-    return info_for_user
-    
-def get_word(word_list):
-    random_word = random.choice(word_list)
-    word_length = (len(random_word) * ("_ "))
-    print(word_length)
-    return random_word
-
-def guessing_letter():
-    pass
-    
-def make_list_from_text(words):
-    list_of_words = words.split()
-    return list_of_words
-
 def read_file(filename):
     with open(filename) as words:
         text = words.read()
         return make_list_from_text(text)
 
+def make_list_from_text(words):
+    list_of_words = words.split()
+    return list_of_words
+    
+def get_word(word_list):
+    random_word = random.choice(word_list)
+    word_length = (("_ ") * len(random_word))
+    print(word_length)
+    return random_word
+    
+def guessed_letter():
+    correct_guesses = []
+    incorrect_guesses = []
+    request = input("\nPlease enter one letter that you think is in the word, and then press Enter.\n")
+    if request in word(get_word):
+        correct_guesses.append(request)
+        print("Awesome!  Guess again!")
+    else: 
+        incorrect_guesses.append(request)
+        print("Sorry, try again.")
+
+def playing_game(word):
+    gameon = True
+    while gameon and len(incorrect_guesses)<8:
+        check_correct_guesses(word, correct_guesses, incorrect_guesses)
+    print(correct_guesses)
+    return correct_guesses
+
+print("\nWelcome to the Mystery Word game. This is a game where you will guess the letters to spell a word. Let's start!\n")
+
 get_word(read_file("words.txt"))
-info_for_user()
+guessed_letter()
+playing_game()
+
+# return input where you ask it 
+# var = function(input)
+#     and then the response to the input is that var
